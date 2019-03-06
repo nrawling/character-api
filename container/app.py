379aeec50977942d695609d789e888b9a6589a87
Nathan Rawling <nathan.rawling@gmail.com>
2019-03-05 22:38:32 -0500

    Started integrating with MySQL backend.

--- container/app.py
+++ container/app.py
@@ -1,17 +1,20 @@
 # loosely based on  https://codeburst.io/this-is-how-easy-it-is-to-create-a-rest-api-8a25122ab1f3
 
+import os
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 api = Api(app)
 
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = ''
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = ''
-app.config['MYSQL_DATABASE_HOST'] = ''
+app.config['MYSQL_DATABASE_USER'] = os.environ.get('MYSQL_DATABASE_USER', None)
+app.config['MYSQL_DATABASE_PASSWORD'] = os.environ.get('MYSQL_DATABASE_PASSWORD', None)
+app.config['MYSQL_DATABASE_DB'] = os.environ.get('MYSQL_DATABASE_DB')
+app.config['MYSQL_DATABASE_HOST'] = os.environ.get('MYSQL_DATABASE_HOST')
 
+mysql = MySQL()
 mysql.init_app(app)
 
 characters = [
@@ -25,10 +28,14 @@ characters = [
 
 class Character(Resource):
 	def get(self, name):
-		for character in characters:
-			if(name == character["name"]):
-				return character, 200
-		return "Character not found", 404
+		try:
+			conn = mysql.connect()
+			cursor = conn.cursor()
+			cursor.execute("SELECT * FROM characters WHERE character_name = %s",name)
+			data = cursor.fetchone()
+			return data, 200
+		except:
+			return "Character not found", 404
 
 	def post(self, name):
 		parser = reqparse.RequestParser()
@@ -37,18 +44,28 @@ class Character(Resource):
 		parser.add_argument("level")
 		args = parser.parse_args()
 
-		for character in characters:
-			if(name == character["name"]):
-				return "Character with name {} already exists".format(name), 400
+		try:
+			conn = mysql.connect()
+			cursor = conn.cursor()
+			cursor.execute("SELECT * FROM characters WHERE character_name = %s",name) 
+			data = cursor.fetchall()
+			if len(data) > 0:
+				return data, 200
+		except:
+			"Unable to connect to database!", 404
 
-		character = {
-			"name": name,
-			"race": args["race"],
-			"class": args["class"],
-			"level": args["level"]
-		}
-		characters.append(characters)
-		return character, 201
+		try:
+			conn = mysql.connect()
+			cursor = conn.cursor()
+
+			cursor.execute("INSERT INTO characters (character_name,character_race,character_class,character_level) VALUES ('{}','{}','{}',{})".format(name,args["race"],args["class"],int(args["level"])))
+
+			cursor = conn.cursor()
+			cursor.execute("SELECT * FROM characters WHERE character_name = %s",name)
+			data = cursor.fetchone()
+			return data, 201
+		except:
+			return "Unable to INSERT new character!", 404
 
 	def put(self, name):
 		parser = reqparse.RequestParser()
